a8024dd5ace83051c7e55dd2401cb6db73a14133
Cesar <cesarnsingi@hotmail.com>
2024-11-26 14:09:30 +0100

    game function

---
+++ src/Game.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Game() {
+  return <div> Tic Tac Toe Game Page</div>
+
+}
+
+export default Game
