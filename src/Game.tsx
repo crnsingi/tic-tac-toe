f3b7888b7d5f89e70aa522304ddf08d727fe15b8
Cesar <cesarnsingi@hotmail.com>
2024-11-26 21:00:28 +0100

    add root

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,6 +1,6 @@
 function Game() {
   return ( 
-  <div className="h-full p-8 text-slate-800 bg-gradient-from-r from-cyan-500 to-blue-500">
+  <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
      <h1 className="text-center text-5xl mb-4 font-display text-white">
       Tic Tac Toe Game Page
       </h1>
@@ -13,4 +13,4 @@ function Game() {
   );
 }
 
-export default Game
+export default Game;
