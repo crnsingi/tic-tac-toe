8c2e2de233aea91776200f8a68a401bfe04cb02f
Cesar <cesarnsingi@hotmail.com>
2024-11-26 21:02:36 +0100

    add tailwinf conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["Lobster", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
-
+};
