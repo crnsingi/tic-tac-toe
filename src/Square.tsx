4459185386c5ece13aaf9ea390aeb26b7d4a9be1
Cesar <cesarnsingi@hotmail.com>
2024-11-27 16:16:09 +0100

    player span

--- src/Square.tsx
+++ src/Square.tsx
@@ -4,7 +4,7 @@ type Props = {
     
 };
 
-function Square({ children }: Props) {
+function Square({ player }: Props) {
     const scale = player ? "scale-100" : "scale-0"
     const textColor = player === "X" ? "text-yellow-200" : "text-fuchsia-300";
     const hoverStyle = "transition duration-500 hover:scale-105 transform";
@@ -12,7 +12,7 @@ function Square({ children }: Props) {
 
     return (
         <div className="h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer">
-            <span>{children}</span>
+            <span>{player}</span>
         </div>
     );
 }
