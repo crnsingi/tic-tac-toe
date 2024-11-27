2b869a485553284336524a884a210fb4a7585760
Cesar <cesarnsingi@hotmail.com>
2024-11-27 16:46:24 +0100

    data cell index

--- src/Square.tsx
+++ src/Square.tsx
@@ -12,8 +12,15 @@ function Square({ index, onClick, player }: Props) {
     
 
     return (
-        <div className={`h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer ${hoverStyle}`}>
-            <span className={`transform transition-all duration-150 ease-out ${scale} ${textColor}`}>{player}</span>
+        <div 
+        data-cell-index={index}
+        className={`h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer ${hoverStyle}`}
+        >
+            <span 
+            className={`transform transition-all duration-150 ease-out ${scale} ${textColor}`}
+            >
+            {player}
+            </span>
         </div>
     );
 }
