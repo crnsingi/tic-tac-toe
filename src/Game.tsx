f80ebf00484346c84b41a6851691f442ee814fa0
Cesar <cesarnsingi@hotmail.com>
2024-11-27 10:33:38 +0100

    key index

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
-const INITIAL_GAME_STATE = ["X","O","","","","","","",""];
+const INITIAL_GAME_STATE = ["X","X","X","","","","","",""];
 
 function Game() {
-  const [gameState, setGameState] = useState(INITIAL_GAME_STATE)
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+
   return ( 
   <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
      <h1 className="text-center text-5xl mb-4 font-display text-white">
@@ -12,7 +13,7 @@ function Game() {
       <div>
         <div>
         {gameState.map((player, index) => (
-          <div>{player}</div>
+          <div key={index}>{player}</div>
         ))}
         </div>
 
