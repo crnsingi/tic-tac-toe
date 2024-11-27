6c5ffaf4026ebea8b1622e09f70fe72f1b00c1e4
Cesar <cesarnsingi@hotmail.com>
2024-11-27 11:07:10 +0100

    fix conf for unresponsive local

--- src/Game.tsx
+++ src/Game.tsx
@@ -11,9 +11,9 @@ function Game() {
       Tic Tac Toe Game Page
       </h1>
       <div>
-        <div>
+        <div className="grid grid-cols-3 gap-3 mx-auto w-96">
         {gameState.map((player, index) => (
-          <div key={index}>{player}</div>
+          <div key={index} className="h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer">{player}</div>
         ))}
         </div>
 
