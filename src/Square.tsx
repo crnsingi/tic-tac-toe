938debaed48227d83342a9dc708fb1bf9ee3ec74
Cesar <cesarnsingi@hotmail.com>
2024-11-27 16:14:23 +0100

    const hover style

--- src/Square.tsx
+++ src/Square.tsx
@@ -6,6 +6,8 @@ type Props = {
 function Square({ children }: Props) {
     const scale = player ? "scale-100" : "scale-0"
     const textColor = player === "X" ? "text-yellow-200" : "text-fuchsia-300";
+    const hoverStyle = "transition duration-500 hover:scale-105 transform";
+    
 
     return (
         <div className="h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer">
