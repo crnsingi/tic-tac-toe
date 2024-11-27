4d2544e1c37d314db5d32bac2148a7dd218d99d7
Cesar <cesarnsingi@hotmail.com>
2024-11-27 16:10:00 +0100

    const scale

--- src/Square.tsx
+++ src/Square.tsx
@@ -5,6 +5,8 @@ type Props = {
 }
 
 function Square({ children }: Props) {
+    const scale = player ? "scale-100" : "scale-0"
+
     return (
         <div className="h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer">
             <span>{children}</span>
