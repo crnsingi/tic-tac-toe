c59c8f47f5e8d8b9649f9ff6a7205497f8e7bfff
Cesar <cesarnsingi@hotmail.com>
2024-11-27 11:25:26 +0100

    props children

--- src/Square.tsx
+++ src/Square.tsx
@@ -1,10 +1,10 @@
 import { ReactNode } from "react";
 
-type Props ={
+type Props = {
     children: ReactNode
 }
 
-function Square({ children }) {
+function Square({ children }: Props) {
     return (
         <div className="h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer">{children}
         </div>
