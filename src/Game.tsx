988d37899408e6c133f883dddb1ba1371d4d5273
Cesar <cesarnsingi@hotmail.com>
2024-11-27 10:01:37 +0100

    const game state and set game state

--- src/Game.tsx
+++ src/Game.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const INITIAL_GAME_STATE = ["X","O","","","","","","",""];
 
 function Game() {
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE)
   return ( 
   <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
      <h1 className="text-center text-5xl mb-4 font-display text-white">
