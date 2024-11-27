25bde59731c37bbd880a64644c6fc6d835ce93dc
Cesar <cesarnsingi@hotmail.com>
2024-11-27 16:48:25 +0100

    on click

--- src/Square.tsx
+++ src/Square.tsx
@@ -15,6 +15,7 @@ function Square({ index, onClick, player }: Props) {
         <div 
         data-cell-index={index}
         className={`h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer ${hoverStyle}`}
+        {...{ onClick}}
         >
             <span 
             data-cell-index={index}
