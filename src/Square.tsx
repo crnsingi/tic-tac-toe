d898ede1a24500431690eaed2307aa5c09a2c0c7
Cesar <cesarnsingi@hotmail.com>
2024-11-27 16:13:27 +0100

    const text color

--- src/Square.tsx
+++ src/Square.tsx
@@ -1,11 +1,11 @@
-import { ReactNode } from "react";
 
 type Props = {
-    children: ReactNode
+    
 }
 
 function Square({ children }: Props) {
     const scale = player ? "scale-100" : "scale-0"
+    const textColor = player === "X" ? "text-yellow-200" : "text-fuchsia-300";
 
     return (
         <div className="h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer">
