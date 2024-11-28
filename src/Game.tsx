cd45efae0d71a7d0892d0d4ce00c5e07af73cd7b
Cesar <cesarnsingi@hotmail.com>
2024-11-28 10:10:20 +0100

    let b and c game state

--- src/Game.tsx
+++ src/Game.tsx
@@ -30,6 +30,9 @@ function Game() {
       const winCombo = WINNING_COMBOS[i];
 
       let a = gameState[winCombo[0]]
+      let b = gameState[winCombo[1]]
+      let c = gameState[winCombo[2]]
+
 
     }
   };
