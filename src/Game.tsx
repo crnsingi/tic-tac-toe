118f6fc23d533785e03e86e73c1585dc1ac0e5d4
Cesar <cesarnsingi@hotmail.com>
2024-11-28 10:04:19 +0100

    let round won

--- src/Game.tsx
+++ src/Game.tsx
@@ -4,7 +4,14 @@ import Square from "./Square";
 const INITIAL_GAME_STATE = ["","","","","","","","",""];
 
 const WINNING_COMBOS = [
-  
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ]
 
 function Game() {
@@ -17,6 +24,7 @@ function Game() {
   }, [gameState]);
 
   const checkForWinner = () => {
+    let roundWon = false
 
   }
 
